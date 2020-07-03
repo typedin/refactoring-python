1c2c74f53aa21d1fa1ee1d0706329731b6154db8
AntoineC <antoine@a-f.studio>
2020-07-03 22:17:47 +0200

    starting state

--- src/statement.py
+++ src/statement.py
@@ -1,2 +1,42 @@
-def statement():
-    return "FOO"
+from math import floor
+
+
+def statement(invoice, plays):
+    totalAmount = 0
+    volumeCredits = 0
+    result = "Statement for {}".format(invoice['customer'])
+    result += "\n"
+
+    def formatCurrency(aNumber):
+        return '${:,.2f}'.format(aNumber)
+
+    for perf in invoice['performances']:
+        play = plays[perf["playID"]]
+        thisAmount = 0
+
+        if play["type"] == "tragedy":
+            thisAmount = 40000
+            if perf["audience"] > 30:
+                thisAmount += 1000 * (perf["audience"] - 30)
+        elif play["type"] == "comedy":
+            thisAmount = 30000
+            if perf["audience"] > 20:
+                thisAmount += 10000 + 500 * (perf["audience"] - 20)
+            thisAmount += 300 * perf["audience"]
+        else:
+            raise Exception("unknown type: {}".format(play["type"]))
+
+        volumeCredits += max(perf['audience'] - 30, 0)
+
+        if play["type"] == "comedy":
+            volumeCredits += floor(perf["audience"] / 5)
+
+        result += " {}: {} ({} seats)\n".format(
+            play["name"], formatCurrency(thisAmount / 100), perf["audience"])
+
+        totalAmount += thisAmount
+
+    result += "Amount owed is {}\n".format(formatCurrency(totalAmount / 100))
+    result += "You earned {} credits\n".format(volumeCredits)
+
+    return result
--- tests/test_statement.py
+++ tests/test_statement.py
@@ -1,10 +1,26 @@
+import json
 import unittest
+from pprint import pprint
 from src.statement import statement
 
+with open("data/plays.json") as f:
+    plays = json.load(f)
+with open("data/invoices.json") as f:
+    invoices = json.load(f)
 
-class TestAppleSauce(unittest.TestCase):
+
+EXPECTATION = '''Statement for BigCo
+ Hamlet: $650.00 (55 seats)
+ As You Like It: $580.00 (35 seats)
+ Othello: $500.00 (40 seats)
+Amount owed is $1,730.00
+You earned 47 credits
+'''
+
+
+class TestStatement(unittest.TestCase):
     def test_sanity(self):
-        self.assertEqual("FOO", statement())
+        self.assertMultiLineEqual(EXPECTATION, statement(invoices[0], plays))
 
 
 if __name__ == '__main__':
